a0d9f490038e642d97de144999e785f3d0e2f03e
surilindur <16085353+surilindur@users.noreply.github.com>
2026-03-05 16:06:41 +0100

    Add Vite plugin to generate type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { compilerOptions } from './tsconfig.json'
 import { defineConfig } from 'vite'
+import dts from 'unplugin-dts/vite'
 
 export default defineConfig({
   build: {
     lib: {
       entry: 'lib/index.ts',
       fileName: 'index',
+      formats: [ 'umd', 'es' ],
       name: 'index'
     },
-    minify: true,
     sourcemap: true,
     target: compilerOptions.target
-  }
+  },
+  plugins: [
+    dts({
+      include: [
+        'lib/*.ts'
+      ]
+    })
+  ]
 })
