c08f9c93c16cef9be4e331b331ff1df35c003bd8
surilindur <16085353+surilindur@users.noreply.github.com>
2026-03-19 13:29:48 +0100

    Use @stylistic/eslint-plugin for linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,30 +1,35 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import { configs } from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { configs as jsConfigs } from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import { configs as tsConfigs } from 'typescript-eslint';
 
 export default defineConfig(
-  configs.all,
-  tseslint.configs.eslintRecommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
   globalIgnores([
-    '**/*.d.ts',
-    '**/*.js',
-    '**/*.js.map',
-    '**/coverage/',
-    '**/node_modules/'
+    '.yarn',
+    'coverage',
+    'node_modules',
+    'old',
+    'old_packages'
   ]),
+  jsConfigs.recommended,
+  tsConfigs.eslintRecommended,
+  tsConfigs.recommended,
+  tsConfigs.strict,
+  stylistic.configs.customize({
+    commaDangle: 'never',
+    indent: 2,
+    jsx: false,
+    quotes: 'single',
+    semi: true,
+    quoteProps: 'as-needed'
+  }),
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    files: ['**/*.ts'],
     rules: {
-      'array-bracket-spacing': [ 'error', 'always' ],
-      'comma-dangle': [ 'error', 'never' ],
-      'computed-property-spacing': [ 'error', 'never' ],
-      // eslint-disable-next-line no-magic-numbers
-      'indent': [ 'error', 2, { SwitchCase: 1 } ],
-      'object-curly-spacing': [ 'error', 'always' ],
-      'one-var': [ 'error', 'never' ],
-      'quotes': [ 'error', 'single' ],
-      'semi': [ 'error', 'never' ]
+      '@typescript-eslint/no-dynamic-delete': 'off'
     }
   }
-)
+);
